54fe8b7c37d6d57265021e2f8d7e7d1b263945ef
JayMtz <jxspell@gmail.com>
2023-12-10 23:23:32 -0600

    hiding sensative info

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,20 @@
+require('dotenv').config();
+import { env } from 'node:process';
 import { Injectable } from '@nestjs/common';
 import {createPool } from 'mysql2/promise';
+
+const DATABASE_PASSWORD = env.DATABASE_PASSWORD
+const DATABASE_USER = env.DATABASE_USER
+const DATABASE_HOST = env.DATABASE_HOST
+const DATABASE_NAME = env.DATABASE_NAME
 
 @Injectable()
 export class UsersService {
   private pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1194',
-    database: 'SpotifyUploader',
+    host: DATABASE_HOST,
+    user: DATABASE_USER,
+    password: DATABASE_PASSWORD,
+    database: DATABASE_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
